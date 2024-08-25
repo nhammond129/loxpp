e17d9228ed1d92b7d60a074d00d2f7bf4a2822eb
Auralyn Hammond <nhammond129@gmail.com>
2024-08-25 16:28:22 -0500

    parser wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <regex>
+#include "parser.hpp"
+
+namespace TestCases {
+    constexpr std::string source = "(){}.,+;-*";
+    const Token expected[] = {
+        Token { Token::Type::LPAREN   , "(", 1 },
+        Token { Token::Type::RPAREN   , ")", 1 },
+        Token { Token::Type::LBRACE   , "{", 1 },
+        Token { Token::Type::RBRACE   , "}", 1 },
+        Token { Token::Type::DOT      , ".", 1 },
+        Token { Token::Type::COMMA    , ",", 1 },
+        Token { Token::Type::PLUS     , "+", 1 },
+        Token { Token::Type::SEMICOLON, ";", 1 },
+        Token { Token::Type::ASTERISK , "*", 1 },
+        Token { Token::Type::MINUS    , "-", 1 },
+        Token { Token::Type::EndOfFile, "",  1 }
+    };
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
---
+++ src/parser.hpp
@@ -0,0 +1,135 @@
+#include <fstream>
+#include <string>
+#include <regex>
+#include <list>
+#include <variant>
+#include <optional>
+
+class Identifier {
+public:
+    constexpr Identifier(std::string name) : name(name) {}
+    std::string name;
+};
+
+struct Literal {
+    std::variant<Identifier, std::string, double> value;
+    Literal(Identifier value) : value(value) {}
+    Literal(std::string value) : value(value) {}
+    Literal(double value) : value(value) {}
+
+    operator std::string() const {
+        return std::visit([](auto&& arg) -> std::string {
+            using T = std::decay_t<decltype(arg)>;
+            if constexpr (std::is_same_v<T, Identifier>) {
+                return arg.name;
+            } else if constexpr (std::is_same_v<T, std::string>) {
+                return arg;
+            } else if constexpr (std::is_same_v<T, double>) {
+                return std::to_string(arg);
+            }
+        }, value);
+    }
+};
+
+class Token {
+public:
+    enum class Type {
+        // Single-char tokens
+        LPAREN, RPAREN, LBRACE, RBRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, ASTERISK,
+
+        // One or two char tokens
+        NOT, NOTEQ,
+        EQ , EQEQ,
+        GT , GTEQ,
+        LT , LTEQ,
+
+        // Literals
+        IDENTIFIER, STRING, NUMBER,
+
+        // Keywords
+        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+        // EOF
+        EndOfFile
+    };
+
+    const Type type;
+    const std::string lexeme;
+    const std::optional<Literal> literal;
+    const size_t line_no;
+
+    Token(Type type, std::string lexeme, std::optional<Literal> literal, size_t line_no)
+        : type(type), lexeme(lexeme), literal(literal), line_no(line_no) {}
+    Token(Type type, std::string lexeme, size_t line_no)
+        : type(type), lexeme(lexeme), literal(std::nullopt), line_no(line_no) {}
+
+    operator std::string() const {
+        if (literal.has_value()) {
+            return std::to_string(static_cast<unsigned int>(type)) + " " + lexeme + " " + std::string{ *literal };
+        } else {
+            return std::to_string(static_cast<unsigned int>(type)) + " " + lexeme;
+        }
+    }
+
+};
+
+class Scanner {
+public:
+    Scanner(const std::string& source) : source(source) {}
+
+    void scan_token() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(Token::Type::LPAREN   , "("); break;
+            case ')': addToken(Token::Type::RPAREN   , ")"); break;
+            case '{': addToken(Token::Type::LBRACE   , "{"); break;
+            case '}': addToken(Token::Type::RBRACE   , "}"); break;
+            case ',': addToken(Token::Type::COMMA    , ","); break;
+            case '.': addToken(Token::Type::DOT      , "."); break;
+            case '-': addToken(Token::Type::MINUS    , "-"); break;
+            case '+': addToken(Token::Type::PLUS     , "+"); break;
+            case ';': addToken(Token::Type::SEMICOLON, ";"); break;
+            case '*': addToken(Token::Type::ASTERISK , "*"); break;
+        }
+    }
+
+    std::list<Token> scan_tokens() {
+        while (!at_end()) {
+            start = current;
+            scan_token();
+        }
+
+        tokens.push_back(Token(Token::Type::EndOfFile, "", line));
+        return tokens;
+    }
+
+    void addToken(Token::Type type) {
+        addToken(type, "");
+    }
+
+    void addToken(Token::Type type, std::string lexeme) {
+        tokens.push_back(Token {type, lexeme, line});
+    }
+
+    char advance() {
+        if (at_end()) return '\0';
+        char c = source[current++];
+        linepos++;
+        if (c == '\n') {
+            linepos = 0;
+            line++;
+        }
+        return c;
+    }
+
+    bool at_end() { return current >= source.size(); }
+
+private:
+    std::string source;
+    size_t start, current;
+    size_t line;
+    size_t linepos;
+    std::list<Token> tokens;
+};
